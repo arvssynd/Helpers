6b2f4ed6879a957de45c6f9c2d5f17945d7bcaf4
Giacomo Caldironi <giacomo.caldironi@dgroove.it>
2020-10-23 10:30:59 +0200

    Add project files.

---
+++ Helpers/ExcelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Helpers
+{
+    public class ExcelGenerator
+    {
+        /// <summary>
+        /// Generate a csv file on the fly
+        /// </summary>
+        public void GenerateCSVFromList()
+        {
+            var users = new List<User>();
+            using (StreamWriter sw = File.CreateText("list.csv"))
+            {
+                foreach (var item in users)
+                {
+                    sw.WriteLine(item.GivenName + ";" + item.Surname + ";" + item.Mail);
+                }
+            }
+        }
+    }
+
+    public class User
+    {
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+    }
+}
